c7536f4d02bed6ace92ec1d3c9d6a529e7c20eb5
mervy <rgrsoares@yahoo.com.br>
2021-11-05 11:04:33 -0300

    Back to top new version

--- backToTop/assets/js/custom.js
+++ backToTop/assets/js/custom.js
@@ -1,16 +1,22 @@
-jQuery(document).ready(function() {
-            // Exibe ou oculta o botão
-            jQuery(window).scroll(function() {
-                if (jQuery(this).scrollTop() > 200) {
-                    jQuery('.backtotop').fadeIn(200);
-                } else {
-                    jQuery('.backtotop').fadeOut(200);
-                }
-            });
-            
-            // Faz animação para subir
-            jQuery('.backtotop').click(function(event) {
-                event.preventDefault();
-                jQuery('html, body').animate({scrollTop: 0}, 300);
-            })
-        });
+// Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
