9c5ae0b0b54fd973cc7fa9317022b126992fb165
Rogério Soares <rgrsoares@yahoo.com.br>
2021-11-08 16:39:40 -0300

    Tic Tac toe finished

--- projects/tic_tac_toe/assets/js/app.js
+++ projects/tic_tac_toe/assets/js/app.js
@@ -3,38 +3,86 @@
 const statusDisplay = document.querySelector(".game-status");
 let gameActive = true;
 let currentPlayer = "X";
-let gameState = ["","","","","","","","",""];
-const winningMessage = () =>`Player ${currentPlayer} has won!`;
+let gameState = ["", "", "", "", "", "", "", "", ""];
+const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 //Start the game
 statusDisplay.innerHTML = currentPlayerTurn();
 
 
 //Functions
-function handleCellPlayed(){
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clicked.innerHTML = currentPlayer;
+}
 
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
-function handlePlayerChange(){
-    
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; $i++){
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if(a === '' || b ==== '' || c ==== '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+    if(roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if(roundDraw){
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
 }
-function handleResultValidation(){
-    
-}
-function handleCellClick(clickedCellEvent){
+
+function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(
         clickedCell.getAttribute('data-cell-index')
     );
-    if (gameState[clickedCellIndex] !== "" || !gameActive){
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
         return;
     }
     handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
 }
-function handleRestartGame(){
-    
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll(".cell")
+    .forEach(cell => cell.innerHTML = "");
 }
 //And finally we add our event listeners to the actual game cells, as well as our restart button
 document.querySelectorAll(".cell").forEach(cell => cell.addEventListener("click", handleCellClick));
