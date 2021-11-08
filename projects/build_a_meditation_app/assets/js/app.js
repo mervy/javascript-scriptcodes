b54d76fe42af2da00eef362fbc16fbfbd01d649f
mervy <rgrsoares@yahoo.com.br>
2021-11-08 09:22:24 -0300

    Create project Build a Meditation App

---
+++ projects/build_a_meditation_app/assets/js/app.js
@@ -0,0 +1,14 @@
+const circleProgress = document.querySelector(".circle-progress");
+const numberOfBreaths = document.querySelector(".breath-input");
+const start = document.querySelector(".start");
+const instructions = document.querySelector(".instructions");
+const breathsText = document.querySelector(".breaths-text");
+let breathsLeft = 3;
+
+// Watching for selected breaths from user
+numberOfBreaths.addEventListener("change", () => {
+  breathsLeft = numberOfBreaths.value;
+  breathsText.innerText = breathsLeft;
+});
+
+//Grow/Shrink Circle
