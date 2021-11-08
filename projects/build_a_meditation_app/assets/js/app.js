6e89ab175a6b38faf00f50a9c4ed4431a2f690eb
mervy <rgrsoares@yahoo.com.br>
2021-11-08 11:02:51 -0300

    Create project Build a Meditation App v.2

--- projects/build_a_meditation_app/assets/js/app.js
+++ projects/build_a_meditation_app/assets/js/app.js
@@ -12,3 +12,60 @@ numberOfBreaths.addEventListener("change", () => {
 });
 
 //Grow/Shrink Circle
+const growCircle = () => {
+  circleProgress.classList.add("circle-grow");
+  setTimeout(() => {
+    circleProgress.classList.remove("circle-grow");
+  }, 8000);
+};
+
+//Breathing Instructions
+const breathTextUpdate = () => {
+  breathsLeft--;
+  breathsText.innerText = breathsLeft;
+  instructions.innerText = "Breath in";
+  setTimeout(() => {
+    instructions.innerText = "Hold breath";
+    setTimeout(() => {
+      instructions.innerText = "Exhale Breath Slowly";
+    }, 4000);
+  }, 4000);
+}
+
+//Breathing App Function 
+const breathingApp = () => {
+  const breathingAnimation = setInterval(() => {
+    if (breathsLeft === 0) {
+      clearInterval(breathingAnimation);
+      instructions.innerText = "Breathing session completed. Click 'Begin' to start another session!";
+      start.classList.remove("button-inactive");
+      breathsLeft = numberOfBreaths.value;
+      breathsText.innerText = breathsLeft;
+      return;
+    }
+    growCircle();
+    breathTextUpdate();
+  }, 12000);
+}
+
+//Start/Stop Sound
+const playSong = () => {
+  let audio = new Audio("../song/abundance.mp3");
+  audio.play();
+};
+
+//Start Breathing
+start.addEventListener("click", () => {
+  start.classList.add("button-inactive");
+  instructions.innerText = "Get relaxed, and ready to begin breatthing";
+  setTimeout(() => {
+    instructions.innerText = "Breathing is about to begin ...";
+    setTimeout(() => {
+      playSong();
+      breathingApp();
+      growCircle();
+      breathTextUpdate();
+    }, 2000);
+  }, 2000);
+
+});
