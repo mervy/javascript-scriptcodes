b4787bc8f49eca294521efcdd000485031198896
mervy <rgrsoares@yahoo.com.br>
2021-11-10 11:56:52 -0300

    Project Slideshow finished

--- back-to-top/assets/js/custom.js
+++ back-to-top/assets/js/custom.js
@@ -1,5 +1,5 @@
 // Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let mybutton = document.getElementById("back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
