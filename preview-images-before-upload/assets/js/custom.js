5fe73cffc2a012044945d35505045626f0e08c5c
mervy <rgrsoares@yahoo.com.br>
2021-11-12 12:18:36 -0300

    Improved CSS preview images e others

--- preview-images-before-upload/assets/js/custom.js
+++ preview-images-before-upload/assets/js/custom.js
@@ -1,8 +1,21 @@
 /*Preview de Imagens antes do Upload
+<input type="file" accept="image/*" onchange="loadFile(event)" > 
+<img id="output" width="100px" /> <br>
+*/
+var loadFile = function (event) {
+    var output = document.getElementById('previewImageFrontCover');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.style.width = '213px';
+    output.style.height = '120px';    
+    output.onload = function () {
+        URL.revokeObjectURL(output.src) // free memory        
+    }
+};
+
+/*Preview de Imagens antes do Upload
 <input type="file" name="files[]" id="inputMultipleImagesPreview"> 
 <div id="galleryPreview"></div>
 */
-
 $(document).ready(function () {
     $('#inputMultipleImagesPreview').change(function () {
         $("#galleryPreview").html('');
@@ -14,15 +27,4 @@ $(document).ready(function () {
 });
 
 
-/*Preview de Imagens antes do Upload
-<input type="file" accept="image/*" onchange="loadFile(event)" > 
-<img id="output" width="100px" /> <br>
-*/
-var loadFile = function (event) {
-    var output = document.getElementById('previewImageFrontCover');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function () {
-        URL.revokeObjectURL(output.src) // free memory
-    }
-};
 
